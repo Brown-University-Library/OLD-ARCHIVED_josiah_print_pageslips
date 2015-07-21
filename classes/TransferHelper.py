5a81b19709ce2fdb342290524359d0a7e9d8bad5
birkin <birkin.diana@gmail.com>
2015-07-21 15:25:36 -0400

    refactored with sub-functions.

--- classes/TransferHelper.py
+++ classes/TransferHelper.py
@@ -4,7 +4,7 @@
 
 from __future__ import unicode_literals
 
-import logging, re, string
+import logging, re
 
 
 log = logging.getLogger(__name__)
@@ -29,27 +29,15 @@ class FileNumberGrabber( object ):
         self.found_file_name = self._find_file_name( screen_text )
         if not self.found_file_name:
             return '-1'
-
-        # find position of fileName and deduce fileName number
-
-        if( returnVal != "-1" ):
-            foundFileNamePosition = string.find( screen_text, self.found_file_name )
-            numberStartPosition = foundFileNamePosition - 5
-            textSectionA = screen_text[numberStartPosition:numberStartPosition+3]
-            textSectionB = string.strip(textSectionA)  # removes leading and trailing whitespace
-            if(textSectionB[0:1] == 'H'):  # removes possible 'H' character
-                textSectionC = textSectionB[1:]
-            else:
-                textSectionC = textSectionB
-            returnVal = textSectionC
-
-        return returnVal
+        file_number = self._determine_file_number( screen_text )
+        log.debug( 'file_number, `%s`' % file_number )
+        return file_number
 
     def _find_file_name( self, screen_text ):
         """ Searches for filename by regex & sets it.
             Called by grab_file_number() """
-        regexPattern = """
-            (jta_20)            # initial prefix
+        regex_pattern = """
+            (jta_20)        # initial prefix
             [0-9][0-9]      # rest of year
             [0-9][0-9]      # month
             [0-9][0-9]      # day
@@ -59,10 +47,24 @@ class FileNumberGrabber( object ):
             [0-9][0-9]      # second
             (\.)(p)         # suffix
             """
-        searchResult = re.search(regexPattern, screen_text, re.VERBOSE)
-        if searchResult:
-            self.found_file_name = searchResult.group()
+        search_result = re.search( regex_pattern, screen_text, re.VERBOSE )
+        if search_result:
+            self.found_file_name = search_result.group()
         return self.found_file_name
+
+    def _determine_file_number( self, screen_text ):
+        """ Finds and returns file-number given file-name.
+            Called by grab_file_number() """
+        foundFileNamePosition = screen_text.find( self.found_file_name )
+        numberStartPosition = foundFileNamePosition - 5
+        textSectionA = screen_text[numberStartPosition:numberStartPosition+3]
+        textSectionB = textSectionA.strip()  # removes leading and trailing whitespace
+        if(textSectionB[0:1] == 'H'):  # removes possible 'H' character
+            textSectionC = textSectionB[1:]
+        else:
+            textSectionC = textSectionB
+        return textSectionC
+
 
 
     # def grab_file_number( self, screen_text ):
