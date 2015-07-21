291f644a0ac03892ec5a32241b28a7154cffd793
birkin <birkin.diana@gmail.com>
2015-07-21 14:39:51 -0400

    adds initial Transfer-Helper class and test.

--- classes/TransferHelper.py
+++ classes/TransferHelper.py
@@ -10,8 +10,118 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class AAA( object ):
+class FileNumberGrabber( object ):
 
-    def bbb( self, screen_text ):
-        """ boo """
-        pass
+    def __init__( self ):
+        self.found_file_name = None
+
+    def grab_file_number( self, screen_text ):
+        """ Grabs the integer representing the file to FTP to the LAS server.
+            Called by FileTransferController.run_code() """
+        # find fileName
+        # find position of fileName
+        # backup 5 positions
+        # capture next 3 characters
+        # remove possible trailing space
+        # remove possible preceeding 'H'
+        # what's left is our number
+
+        import re
+        import string
+
+        # find fileName
+
+        regexPattern = """
+            (jta_20)            # initial prefix
+            [0-9][0-9]      # rest of year
+            [0-9][0-9]      # month
+            [0-9][0-9]      # day
+            (_)             # separator
+            [0-9][0-9]      # hour
+            [0-9][0-9]      # minute
+            [0-9][0-9]      # second
+            (\.)(p)         # suffix
+            """
+
+        searchResult = re.search(regexPattern, screen_text, re.VERBOSE)
+
+        fileName = "init"
+        returnVal = "init"
+
+        if( searchResult == None):
+            returnVal = "-1"
+        else:
+            fileName = searchResult.group()
+            self.found_file_name = fileName
+
+        # find position of fileName and deduce fileName number
+
+        if( returnVal != "-1" ):
+            foundFileNamePosition = string.find( screen_text, fileName )
+            numberStartPosition = foundFileNamePosition - 5
+            textSectionA = screen_text[numberStartPosition:numberStartPosition+3]
+            textSectionB = string.strip(textSectionA)  # removes leading and trailing whitespace
+            if(textSectionB[0:1] == 'H'):  # removes possible 'H' character
+                textSectionC = textSectionB[1:]
+            else:
+                textSectionC = textSectionB
+            returnVal = textSectionC
+
+        return returnVal
+
+    # end class FileNumberGrabber
+
+
+
+    # def determineFileNumber(self, screenText):
+
+    #     # find fileName
+    #     # find position of fileName
+    #     # backup 5 positions
+    #     # capture next 3 characters
+    #     # remove possible trailing space
+    #     # remove possible preceeding 'H'
+    #     # what's left is our number
+
+    #     import re
+    #     import string
+
+    #     # find fileName
+
+    #     regexPattern = """
+    #         (jta_20)            # initial prefix
+    #         [0-9][0-9]      # rest of year
+    #         [0-9][0-9]      # month
+    #         [0-9][0-9]      # day
+    #         (_)             # separator
+    #         [0-9][0-9]      # hour
+    #         [0-9][0-9]      # minute
+    #         [0-9][0-9]      # second
+    #         (\.)(p)         # suffix
+    #         """
+
+    #     searchResult = re.search(regexPattern, screenText, re.VERBOSE)
+
+    #     fileName = "init"
+    #     returnVal = "init"
+
+    #     if( searchResult == None):
+    #         returnVal = "-1"
+    #     else:
+    #         fileName = searchResult.group()
+    #         self.foundFileName = fileName
+
+    #     # find position of fileName and deduce fileName number
+
+    #     if( returnVal != "-1" ):
+    #         foundFileNamePosition = string.find( screenText, fileName )
+    #         numberStartPosition = foundFileNamePosition - 5
+    #         textSectionA = screenText[numberStartPosition:numberStartPosition+3]
+    #         textSectionB = string.strip(textSectionA)  # removes leading and trailing whitespace
+    #         if(textSectionB[0:1] == 'H'):  # removes possible 'H' character
+    #             textSectionC = textSectionB[1:]
+    #         else:
+    #             textSectionC = textSectionB
+    #         returnVal = textSectionC
+
+    #     return returnVal
---
+++ tests/TransferHelperTest.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tests josiah_print_pageslips.classes.TransferHelper
+"""
+
+from __future__ import unicode_literals
+
+import logging, os, unittest
+from josiah_print_pageslips.classes.TransferHelper import FileNumberGrabber
+
+
+## settings from env/activate
+LOG_PATH = os.environ['PGSLP__LOG_PATH']
+LOG_LEVEL = os.environ['PGSLP__LOG_LEVEL']  # 'DEBUG' or 'INFO'
+
+
+## logging
+log_level = { 'DEBUG': logging.DEBUG, 'INFO': logging.INFO }
+logging.basicConfig(
+    filename=LOG_PATH, level=log_level[LOG_LEVEL],
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S'
+    )
+logger = logging.getLogger(__name__)
+
+
+class FileNumberGrabberTest( unittest.TestCase ):
+
+    def test_grab_file_number__good_input(self):
+        """ Tests file-number and file-name grab from good data. """
+        grabber = FileNumberGrabber()
+        screen_text = '[21;57H[4h [4lf[21;58H[1;1H[K[2;1H[K[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;18H        Loading program you requested[K[14;1H[K[15;1H[K[16;1H[K[17;1H[K[18;1H[K[19;1H[K[20;1H[K[21;1H[K[22;1H[K[14;41H[H[2J[1;20HSend print files out of INNOPAC using FTP[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;1H[K[14;1H[K[15;1H[K[3;1H1 > jta_20060328_170905.p[4;1H2 > blahFile.p[5;1H3 > test.p[16;1H[K(0lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk[17;1H[Kx(BF > FTP a print file to another system[17;79H(0x[18;1H[Kx(BR > REMOVE files[18;79H(0x[19;1H[Kx(BY > DISPLAY file SIZE & DATE[19;79H(0x[20;1H[Kx(BQ > QUIT[20;79H(0x[21;1H[Kx(BChoose one (F,R,Y,Q)[21;79H(0x[22;1H[Kx[22;79Hx[23;1H[Kx[23;79Hx[24;1H[Kmqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj(B[21;23H[?1l>[21;23H'
+        result = grabber.grab_file_number( screen_text )
+        self.assertEqual( unicode, type(result) )
+        self.assertEqual( '1', result )
+        self.assertEqual( 'jta_20060328_170905.p', grabber.found_file_name )
+
+
+#     def testDetermineFileNumber_goodInput_fileNameA(self):
+#         """ Tests file-number and file-name grab from good data. """
+
+#         determinerInstance = NumberDeterminer.NumberDeterminer()
+
+#         textOfScreen = '[21;57H[4h [4lf[21;58H[1;1H[K[2;1H[K[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;18H        Loading program you requested[K[14;1H[K[15;1H[K[16;1H[K[17;1H[K[18;1H[K[19;1H[K[20;1H[K[21;1H[K[22;1H[K[14;41H[H[2J[1;20HSend print files out of INNOPAC using FTP[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;1H[K[14;1H[K[15;1H[K[3;1H1 > jta_20060328_170905.p[4;1H2 > blahFile.p[5;1H3 > test.p[16;1H[K(0lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk[17;1H[Kx(BF > FTP a print file to another system[17;79H(0x[18;1H[Kx(BR > REMOVE files[18;79H(0x[19;1H[Kx(BY > DISPLAY file SIZE & DATE[19;79H(0x[20;1H[Kx(BQ > QUIT[20;79H(0x[21;1H[Kx(BChoose one (F,R,Y,Q)[21;79H(0x[22;1H[Kx[22;79Hx[23;1H[Kx[23;79Hx[24;1H[Kmqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj(B[21;23H[?1l>[21;23H'
+# #       fileName = 'jta_sep01_0947'
+
+#         expected = str(1)
+#         returned = determinerInstance.determineFileNumber(textOfScreen)
+#         self.assertEqual( expected, returned, "returned is: " + str(returned) )
+
+#         expected = "jta_20060328_170905.p"
+#         returned = determinerInstance.foundFileName
+#         self.assertEqual( expected, returned, "returned is: " + str(returned) )
+
+
+
+    def testDetermineFileNumber_noFileFound(self):
+        """determines the integer representing a file to FTP to sulu"""
+
+        determinerInstance = NumberDeterminer.NumberDeterminer()
+
+        textOfScreen = '[21;57H[4h [4lf[21;58H[1;1H[K[2;1H[K[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;18H        Loading program you requested[K[14;1H[K[15;1H[K[16;1H[K[17;1H[K[18;1H[K[19;1H[K[20;1H[K[21;1H[K[22;1H[K[14;41H[H[2J[1;20HSend print files out of INNOPAC using FTP[3;1H[K[4;1H[K[5;1H[K[6;1H[K[7;1H[K[8;1H[K[9;1H[K[10;1H[K[11;1H[K[12;1H[K[13;1H[K[14;1H[K[15;1H[K[3;1H1 > blahFile_A.p[4;1H2 > blahFile_B.p[5;1H3 > test.p[16;1H[K(0lqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqk[17;1H[Kx(BF > FTP a print file to another system[17;79H(0x[18;1H[Kx(BR > REMOVE files[18;79H(0x[19;1H[Kx(BY > DISPLAY file SIZE & DATE[19;79H(0x[20;1H[Kx(BQ > QUIT[20;79H(0x[21;1H[Kx(BChoose one (F,R,Y,Q)[21;79H(0x[22;1H[Kx[22;79Hx[23;1H[Kx[23;79Hx[24;1H[Kmqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqj(B[21;23H[?1l>[21;23H'
+#       fileName = 'jta_sep01_0947'
+
+        expected = "-1"
+        returned = determinerInstance.determineFileNumber(textOfScreen)
+        self.assertEqual( expected, returned, "returned is: " + str(returned) )
+
+        expected = ""
+        returned = determinerInstance.foundFileName
+        self.assertEqual( expected, returned, "returned is: " + str(returned) )
+
+
+    # end class FileNumberGrabberTest
+
+
+if __name__ == "__main__":
+    unittest.main()
