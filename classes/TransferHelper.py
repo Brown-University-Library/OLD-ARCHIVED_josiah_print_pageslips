cd3b6bd80d93e47f597aaeb16ea131eb26344821
birkin <birkin.diana@gmail.com>
2015-07-21 14:19:15 -0400

    stubs out new helper class.

---
+++ classes/TransferHelper.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+""" Contains helpers for FileTransferController.py """
+
+from __future__ import unicode_literals
+
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+class AAA( object ):
+
+    def bbb( self, screen_text ):
+        """ boo """
+        pass
